faedc0c7812434e634d54f8e259e45ac37254993
kchaitanya954 <kchaitanya954@gmail.com>
2021-03-25 11:40:11 +0530

    opencv creating trackbar

---
+++ opencv_trackbar.py
@@ -0,0 +1,32 @@
+import numpy as np
+import cv2
+
+img = np.zeros((300, 512, 3), np.uint8)
+cv2.namedWindow('image')
+
+
+def nothing(x):
+    print(x)
+
+
+cv2.createTrackbar('B', 'image', 0, 255, nothing)
+cv2.createTrackbar('G', 'image', 0, 255, nothing)
+cv2.createTrackbar('R', 'image', 0, 255, nothing)
+cv2.createTrackbar('switch','image',  0, 1, nothing)
+
+while (1):
+    cv2.imshow('image', img)
+    key = cv2.waitKey(1) & 0xFF
+    if key == 27:
+        break
+
+    b = cv2.getTrackbarPos('B', 'image')
+    g = cv2.getTrackbarPos('G', 'image')
+    r = cv2.getTrackbarPos('R', 'image')
+    s = cv2.getTrackbarPos('switch', 'image')
+    if s == 1:
+        img[:] = [b, g, r]
+    else:
+        img[:] = 255
+
+cv2.destroyAllWindows()
